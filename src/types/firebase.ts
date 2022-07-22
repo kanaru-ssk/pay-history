56e6648c883061d70683a6b35eea622e2a13e04b
kanaru-ssk <2019january.0131@gmail.com>
2022-07-22 23:56:42 +0900

    type定義

--- src/types/firebase.ts
+++ src/types/firebase.ts
@@ -3,16 +3,23 @@ export type User = import("firebase/auth").User;
 export type FieldValue = import("firebase/firestore").FieldValue;
 export type Timestamp = import("firebase/firestore").Timestamp;
 
-export type Question = {
+export type DBUser = {
   docId: string;
   atCreated: Timestamp | FieldValue;
   atUpdated: Timestamp | FieldValue;
-  question: string;
 };
 
-export type Answer = {
+export type Month = {
   docId: string;
   atCreated: Timestamp | FieldValue;
   atUpdated: Timestamp | FieldValue;
-  answer: string;
+  month: string;
+  budget: number;
+  payments: Payment[];
+};
+
+type Payment = {
+  atCreated: Timestamp | FieldValue;
+  atUpdated: Timestamp | FieldValue;
+  price: number;
 };
