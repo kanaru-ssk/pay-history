3965514a5afb42f901144c11f5f2cc42c578dbba
kanaru-ssk <2019january.0131@gmail.com>
2022-07-23 01:22:38 +0900

    型情報修正

--- src/types/firebase.ts
+++ src/types/firebase.ts
@@ -3,11 +3,13 @@ export type User = import("firebase/auth").User;
 export type FieldValue = import("firebase/firestore").FieldValue;
 export type Timestamp = import("firebase/firestore").Timestamp;
 
-export type Month = {
+export type Month = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+
+export type MonthlyData = {
   docId: string;
   atCreated: Timestamp | FieldValue;
   atUpdated: Timestamp | FieldValue;
-  month: string;
+  month: Month;
   budget: number;
   payments: Payment[];
 };
