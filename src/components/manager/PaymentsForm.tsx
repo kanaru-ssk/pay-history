ebe5cf452bcd3eb1fa786f82f96b0d06ce51e9d0
kanaru-ssk <2019january.0131@gmail.com>
2022-07-24 00:00:25 +0900

    不要なコード削除

--- src/components/manager/PaymentsForm.tsx
+++ src/components/manager/PaymentsForm.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/router";
 import type { MonthlyData } from "types/firebase";
 
 import { useAuth } from "hooks/auth";
-import { addPayment, pathToMonth } from "libs/monthlyData";
+import { addPayment } from "libs/monthlyData";
 
 type Props = {
   thisMonthData: MonthlyData;
