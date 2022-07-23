a79893fa122bcf08ef98746e66abf8c68d207b71
kanaru-ssk <2019january.0131@gmail.com>
2022-07-23 17:15:32 +0900

    Managerをコンポーネント分割

---
+++ src/components/Manager/Budget.tsx
@@ -0,0 +1,108 @@
+import { useEffect, useState } from "react";
+
+import type { MonthlyData } from "types/firebase";
+
+import { useAuth } from "hooks/auth";
+import { updateBudget } from "libs/monthlyData";
+import { updateUser } from "libs/user";
+
+type Props = {
+  thisMonthData: MonthlyData;
+};
+
+const Budget = ({ thisMonthData }: Props) => {
+  const { authUser, dbUser } = useAuth();
+
+  const [budget, setBudget] = useState<number>(0);
+  const [totalSpending, setTotalSpending] = useState<number>(0);
+  const [remaining, setRemaining] = useState<number>(0);
+  const [isReady, setIsReady] = useState<boolean>(false);
+
+  // 初回ロード
+  useEffect(() => {
+    if (thisMonthData?.budget) {
+      setBudget(thisMonthData.budget);
+    }
+  }, [thisMonthData]);
+
+  // 支出合計県産
+  useEffect(() => {
+    if (thisMonthData?.payments) {
+      const total: number = thisMonthData.payments.reduce((sum, value) => {
+        return sum + value.price;
+      }, 0);
+      setTotalSpending(total);
+    }
+  }, [thisMonthData?.payments]);
+
+  // 残高計算
+  useEffect(() => {
+    setRemaining(budget - totalSpending);
+  }, [totalSpending, budget]);
+
+  // 予算編集ボタン
+  const onClickEdit = () => {
+    if (isReady) {
+      updateUser(dbUser, budget);
+      updateBudget(authUser, { ...thisMonthData, budget: budget });
+      setIsReady(false);
+    }
+  };
+
+  // 予算入力
+  const onChangeBudget = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const pattern = /^\d*$/;
+    if (pattern.test(e.target.value)) {
+      const toNum = Number(e.target.value);
+      setBudget(toNum);
+      if (0 < toNum && toNum !== thisMonthData.budget) {
+        setIsReady(true);
+      } else {
+        setIsReady(false);
+      }
+    } else {
+      setBudget(0);
+    }
+  };
+
+  return (
+    <table className="my-8 mx-auto table-auto ">
+      <tbody>
+        <tr>
+          <th>予算</th>
+          <td className="w-48 text-right">
+            <input
+              type="text"
+              inputMode="numeric"
+              value={budget}
+              onChange={onChangeBudget}
+              className="w-40 rounded border border-gray text-right"
+            />
+            円
+          </td>
+          <td>
+            <button
+              className={
+                (isReady ? "" : "text-gray") +
+                " rounded border border-gray px-2"
+              }
+              onClick={onClickEdit}
+            >
+              変更
+            </button>
+          </td>
+        </tr>
+        <tr className="border-b">
+          <th>支出</th>
+          <td className="w-48 text-right">{totalSpending}円</td>
+        </tr>
+        <tr>
+          <th>残高</th>
+          <td className="w-48 text-right text-3xl">{remaining}円</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+export default Budget;
---
+++ src/components/Manager/Payments.tsx
@@ -0,0 +1,88 @@
+import { useState } from "react";
+
+import type { MonthlyData } from "types/firebase";
+
+import { useAuth } from "hooks/auth";
+import { addPayment } from "libs/monthlyData";
+
+type Props = {
+  thisMonthData: MonthlyData;
+};
+
+const Payments = ({ thisMonthData }: Props) => {
+  const { authUser } = useAuth();
+
+  const [price, setPrice] = useState<number>(0);
+  const [isReady, setIsReady] = useState<boolean>(false);
+
+  // 支払い追加
+  const onSumitPayment = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (thisMonthData) {
+      addPayment(authUser, thisMonthData.docId, price);
+      setPrice(0);
+    }
+  };
+
+  // 支払い入力
+  const onChangeNewPayment = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const pattern = /^\d*$/;
+    if (pattern.test(e.target.value)) {
+      const toNum = Number(e.target.value);
+      setPrice(toNum);
+      if (0 < toNum) {
+        setIsReady(true);
+      } else {
+        setIsReady(false);
+      }
+    } else {
+      setPrice(0);
+    }
+  };
+
+  return (
+    <>
+      <table className="mx-auto mt-8 w-full table-auto">
+        <thead className="border-b">
+          <tr>
+            <th className="pl-8 text-left">支払い日</th>
+            <th className="pr-8 text-right">金額</th>
+          </tr>
+        </thead>
+        <tbody>
+          {thisMonthData.payments.map((value) => {
+            return (
+              <tr key={value.atCreated.toString()}>
+                <td className="pl-8 text-left">
+                  {value.atCreated.toDate().getMonth() + 1}日
+                  {value.atCreated.toDate().getDate()}日
+                </td>
+                <td className="pr-8 text-right">{value.price}円</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+
+      <form
+        onSubmit={onSumitPayment}
+        className="flex w-full items-center bg-white px-4 py-2"
+      >
+        <input
+          type="text"
+          inputMode="numeric"
+          placeholder="支出額を入力してください。"
+          value={price.toString()}
+          onChange={onChangeNewPayment}
+          className="top-0 h-full w-full flex-1 resize-none overflow-visible rounded-3xl border-2 border-gray px-5 py-3 leading-5"
+        />
+
+        <button className={(isReady ? "" : "text-gray") + " pl-3 text-right"}>
+          追加
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default Payments;
