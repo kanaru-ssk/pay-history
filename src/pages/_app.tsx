790f094cbe0427b17364df4240da77c9861c202c
kanaru-ssk <2019january.0131@gmail.com>
2022-07-23 18:24:41 +0900

    headを全て_appに移動

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,6 +17,25 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="icon" href="/favicon.svg" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="theme-color" content="#232C93" />
+
+        <meta property="og:url" content={process.env.NEXT_PUBLIC_URL} />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content={process.env.NEXT_PUBLIC_URL + "/img/ogp.png"}
+        />
+        <meta property="og:title" content="pay history" />
+        <meta
+          property="og:description"
+          content="カードの予算管理を簡単にしましょう! カード明細は反映に時間がかかってしまい、今いくら使ったのか把握するのは難しいですよね。pay historyを使えば月の予算とその月に使った金額を簡単に管理できます。"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+
+        <title>pay history</title>
+        <meta
+          name="description"
+          content="カードの予算管理を簡単にしましょう! カード明細は反映に時間がかかってしまい、今いくら使ったのか把握するのは難しいですよね。pay historyを使えば月の予算とその月に使った金額を簡単に管理できます。"
+        />
       </Head>
 
       <Header />
