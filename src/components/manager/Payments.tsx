2ee731ed2e7926b9dcb801c6fabc46a280574ab1
kanaru-ssk <2019january.0131@gmail.com>
2022-07-24 09:39:49 +0900

    ソート機能追加

--- src/components/manager/Payments.tsx
+++ src/components/manager/Payments.tsx
@@ -4,16 +4,47 @@ import EditModal from "./EditModal";
 
 import type { MonthlyData, Payment } from "types/firebase";
 
-import { useAuth } from "hooks/auth";
-
 type Props = {
   thisMonthData: MonthlyData;
 };
 
 const Payments = ({ thisMonthData }: Props) => {
-  const { authUser } = useAuth();
   const [payment, setPayment] = useState<Payment | null>(null);
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [isSortDate, setIsSortDate] = useState<boolean>(false);
+  const [isAcsDate, setIsAcsDate] = useState<boolean>(false);
+  const [isAcsPrice, setIsAcsPrice] = useState<boolean>(false);
+
+  const sortDate = (a: Payment, b: Payment): number => {
+    if (isAcsDate) {
+      if (b.atPaied < a.atPaied) {
+        return 1;
+      } else {
+        return -1;
+      }
+    } else {
+      if (a.atPaied < b.atPaied) {
+        return 1;
+      } else {
+        return -1;
+      }
+    }
+  };
+
+  const sortPrice = (a: Payment, b: Payment): number => {
+    if (isAcsPrice) {
+      if (b.price < a.price) {
+        return 1;
+      } else {
+        return -1;
+      }
+    } else {
+      if (a.price < b.price) {
+        return 1;
+      } else {
+        return -1;
+      }
+    }
+  };
 
   const onClickHundler = (_payment: Payment) => {
     setPayment(_payment);
@@ -32,31 +63,50 @@ const Payments = ({ thisMonthData }: Props) => {
       <table className="mx-auto mt-8 w-full table-auto">
         <thead className="border-b">
           <tr>
-            <th className="pl-4 text-left">支出日</th>
-            <th className="pr-4 text-right">金額</th>
+            <th
+              onClick={() => {
+                setIsSortDate(true);
+                setIsAcsDate(!isAcsDate);
+              }}
+              className="cursor-pointer pl-4 text-left"
+            >
+              支出日{isSortDate && (isAcsDate ? "▲" : "▼")}
+            </th>
+            <th
+              onClick={() => {
+                setIsSortDate(false);
+                setIsAcsPrice(!isAcsPrice);
+              }}
+              className="cursor-pointer pr-4 text-right"
+            >
+              {!isSortDate && (isAcsPrice ? "▲" : "▼")}金額
+            </th>
           </tr>
         </thead>
         <tbody>
           {thisMonthData.payments &&
-            thisMonthData.payments.map((value) => {
-              return (
-                <tr key={value.atCreated.toString()}>
-                  <td className="pl-4 text-left">
-                    {value.atPaied.toDate().getMonth() + 1}日
-                    {value.atPaied.toDate().getDate()}日
-                  </td>
-                  <td className="pr-4 text-right">
-                    {value.price.toLocaleString()}円
-                  </td>
-                  <td
-                    onClick={() => onClickHundler(value)}
-                    className="w-4 cursor-pointer"
-                  >
-                    ︙
-                  </td>
-                </tr>
-              );
-            })}
+            thisMonthData.payments
+              .sort(isSortDate ? sortPrice : sortDate)
+              .sort(isSortDate ? sortDate : sortPrice)
+              .map((value) => {
+                return (
+                  <tr key={value.atCreated.toString()} className="h-9">
+                    <td className="pl-4 text-left">
+                      {value.atPaied.toDate().getMonth() + 1}日
+                      {value.atPaied.toDate().getDate()}日
+                    </td>
+                    <td className="pr-4 text-right">
+                      {value.price.toLocaleString()}円
+                    </td>
+                    <td
+                      onClick={() => onClickHundler(value)}
+                      className="w-4 cursor-pointer text-sm"
+                    >
+                      ︙
+                    </td>
+                  </tr>
+                );
+              })}
           {thisMonthData.payments && thisMonthData.payments.length === 0 && (
             <tr>
               <td className="h-12 text-center text-gray">
