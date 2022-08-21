6735d7e91c987c107cbc48f21359085fa8ce0d6c
kanaru-ssk <2019january.0131@gmail.com>
2022-08-21 23:11:16 +0900

    LP 見た目調整

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -29,23 +29,23 @@ const Introduction = () => {
       </div>
       <h1>Pay History</h1>
       <p>予算管理ツール「Pay History」</p>
-      <Image
-        src="/img/screenshot.png"
-        width={1200}
-        height={630}
-        alt="screenshot"
-      />
       <p className="mb-4">月の予算管理、しっかりできていますか？</p>
       <p className="mb-4">
         クレジットカード明細は反映まで時間がかかってしまい、月初からいくら使ったのか把握するのは難しいですよね。
       </p>
       <p className="mb-8">
         予算管理ツール「Pay History」なら、月の
-        <span className="font-bold text-main-color">予算</span>、使った
-        <span className="font-bold text-main-color">金額</span>、予算
+        <span className="font-bold text-main-color">予算</span>、
+        <span className="font-bold text-main-color">支出額</span>、予算の
         <span className="font-bold text-main-color">残高</span>
         を最もシンプルに管理できます。
       </p>
+      <Image
+        src="/img/screenshot.png"
+        width={992}
+        height={1264}
+        alt="screenshot"
+      />
       <h2>使い方</h2>
       <ol className="list-inside list-decimal">
         <li>月の予算を入力する</li>
