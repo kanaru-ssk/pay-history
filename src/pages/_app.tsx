a7cc3ee2536f5eaf1c79eaa824a387c450dd9aae
kanaru-ssk <2019january.0131@gmail.com>
2022-08-31 20:34:41 +0900

    pwa対応

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,28 +12,8 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <link rel="icon" href="/favicon.svg" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta name="theme-color" content="#C1C5FE" />
-
-        <meta property="og:url" content={process.env.NEXT_PUBLIC_URL} />
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:image"
-          content={process.env.NEXT_PUBLIC_URL + "/img/ogp.png"}
-        />
-        <meta property="og:title" content="Pay History" />
-        <meta
-          property="og:description"
-          content="予算管理ツール「Pay History」なら、月の予算、使った金額、予算残高を最もシンプルに管理できます。"
-        />
-        <meta name="twitter:card" content="summary_large_image" />
-
         <title>Pay History</title>
-        <meta
-          name="description"
-          content="予算管理ツール「Pay History」なら、月の予算、使った金額、予算残高を最もシンプルに管理できます。"
-        />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
 
       <AuthProvider>
