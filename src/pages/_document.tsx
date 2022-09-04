46714c719a4555d1943bd552efecf8e7fb5e1824
kanaru-ssk <2019january.0131@gmail.com>
2022-09-05 00:11:30 +0900

    pwa調整

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,15 +5,43 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="apple-touch-icon" href="/favicon.png" />
-          <link rel="apple-touch-icon-precomposed" href="/favicon.png" />
-          <link rel="icon" type="image/png" href="/favicon.png" />
+          <link
+            rel="apple-touch-icon"
+            type="image/png"
+            sizes="192x192"
+            href="/favicon.png"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            type="image/png"
+            sizes="192x192"
+            href="/favicon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="192x192"
+            href="/favicon.png"
+          />
           <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
           <link rel="manifest" href="/manifest.json" />
-          <link rel="apple-touch-icon" href="/icon.png"></link>
 
-          <meta name="theme-color" content="#fff" />
           <meta name="theme-color" content="#C1C5FE" />
+          <meta name="application-name" content="Pay History" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta name="apple-mobile-web-app-title" content="Pay History" />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta
+            name="msapplication-config"
+            content="/icons/browserconfig.xml"
+          />
+          <meta name="msapplication-TileColor" content="#232C93" />
+          <meta name="msapplication-tap-highlight" content="no" />
+
           <meta property="og:url" content={process.env.NEXT_PUBLIC_URL} />
           <meta property="og:type" content="website" />
           <meta
