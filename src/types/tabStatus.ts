d3463d8f99a010422e4336d64484eedfa81d412d
kanaru-ssk <2019january.0131@gmail.com>
2023-02-16 21:31:39 +0900

    type TabStatusのコード修正

--- src/types/tabStatus.ts
+++ src/types/tabStatus.ts
@@ -1,21 +1,6 @@
-export type TabStatus = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+const monthies = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] as const;
 
-export const isTabStatus = (arg: any): arg is TabStatus => {
-  if (
-    arg === 1 ||
-    arg === 2 ||
-    arg === 3 ||
-    arg === 4 ||
-    arg === 5 ||
-    arg === 6 ||
-    arg === 7 ||
-    arg === 8 ||
-    arg === 9 ||
-    arg === 10 ||
-    arg === 11 ||
-    arg === 12
-  ) {
-    return true;
-  }
-  return false;
-};
+export type TabStatus = (typeof monthies)[number];
+
+export const isTabStatus = (arg: any): arg is TabStatus =>
+  monthies.includes(arg);
