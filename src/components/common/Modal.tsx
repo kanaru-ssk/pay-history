cfc2627a8fe36d8a361c7f25732e5c1f06c05989
kanaru-ssk <2019january.0131@gmail.com>
2023-03-01 12:03:11 +0900

    fix Modal components

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -24,25 +24,23 @@ const Modal = ({ children, isShown, onHide }: Props) => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
   });
-  return (
-    <>
-      {isShown && (
-        <div className="fixed z-20 inset-0 flex items-center justify-center bg-trans-black">
-          <div
-            ref={modalRef}
-            className="max-w-md p-4 overflow-hidden bg-white shadow-xl rounded-xl"
-          >
-            <div className="text-right">
-              <button type="button" onClick={onHide}>
-                <CloseIcon />
-              </button>
-            </div>
 
-            <div className="mt-4">{children}</div>
-          </div>
+  if (!isShown) return null;
+
+  return (
+    <div className="fixed z-20 inset-0 flex items-center justify-center bg-trans-black">
+      <div
+        ref={modalRef}
+        className="max-w-md p-4 overflow-hidden bg-white shadow-xl rounded-xl"
+      >
+        <div className="text-right">
+          <button type="button" onClick={onHide}>
+            <CloseIcon />
+          </button>
         </div>
-      )}
-    </>
+        {children}
+      </div>
+    </div>
   );
 };
 
