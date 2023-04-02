a9e36111b05382266b9299eb6367d235b4af44bd
kanaru-ssk <2019january.0131@gmail.com>
2023-04-03 04:00:31 +0900

    fix text to switch by locale

--- src/components/organisms/Header/Menu/AnonymousMenu.tsx
+++ src/components/organisms/Header/Menu/AnonymousMenu.tsx
@@ -1,18 +1,20 @@
 import Link from "next/link";
+import { useLocale } from "@/hooks/useLocale";
 
 type Props = {
   onClick: () => void;
 };
 
 export const AnonymousMenu = ({ onClick }: Props) => {
+  const { text } = useLocale();
   return (
     <div>
       <Link href="/signUp" onClick={onClick} className="font-bold">
-        Sign Up
+        {text.SIGN_UP}
       </Link>
       <span className="mx-4">/</span>
       <Link href="/signIn" onClick={onClick} className="font-bold">
-        Sign In
+        {text.SIGN_IN}
       </Link>
     </div>
   );
