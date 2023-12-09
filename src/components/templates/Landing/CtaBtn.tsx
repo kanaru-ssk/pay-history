ea63ea00748201f46bd23d2661daab05d63e3b37
kanaru-ssk <2019january.0131@gmail.com>
2023-12-09 20:46:49 +0900

    fix dark mode style

--- src/components/templates/Landing/CtaBtn.tsx
+++ src/components/templates/Landing/CtaBtn.tsx
@@ -6,7 +6,7 @@ export function CtaBtn() {
 
   return (
     <Link href="/" data-cy="cta">
-      <div className="rounded-full bg-black py-2 text-center text-white">
+      <div className="rounded-full bg-black py-2 text-center font-bold text-white dark:bg-gray-200 dark:text-black">
         {text.START_BUDGETING}
       </div>
     </Link>
--- src/components/ui/text/LinkText.tsx
+++ src/components/ui/text/LinkText.tsx
@@ -7,7 +7,10 @@ type Props = {
 
 export function LinkText({ text, href }: Props) {
   return (
-    <Link href={href} className="text-black underline hover:text-gray-400">
+    <Link
+      href={href}
+      className="text-black underline hover:text-gray-400  dark:text-gray-200"
+    >
       {text}
     </Link>
   );
